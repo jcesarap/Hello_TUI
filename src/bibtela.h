e5f7115934a2961eb78ae8269c533fe1d2429978
kaiser <jkaisermp@protonmail.com>
2025-04-25 10:36:43 -0300

    Version 3 & Refactor (forgot to add files)

---
+++ src/bibtela.h
@@ -0,0 +1,27 @@
+// Wrap library dependencies, define function prototypes
+#ifndef _BIBTELA
+#define _BIBTELA
+// The condition above will make sure that libraries are only included if they weren't already
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <unistd.h>
+#include <locale.h>
+#include <string.h>
+#include <wchar.h>
+
+typedef enum {
+    HORIZONTAL,
+    VERTICAL
+} LineDirection;
+
+void init_screen(int x, int y);
+void end_screen(void);
+void clear_screen(void);
+void cursor_to(int x, int y);
+int write_to(int x, int y, const char* str);
+void draw_window(int x1, int y1, int x2, int y2, int border);
+void draw_line(LineDirection direction, int anchor, int start, int end, const char *symbol);
+
+#endif
